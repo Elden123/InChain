7523d4ece514d736a66ce6f1300d3f7a11cbeb15
Unknown <nolanblankenau@gmail.com>
2018-01-28 04:32:09 -0600

    send_sms working

--- send_sms.py
+++ send_sms.py
@@ -1,18 +1,15 @@
 from twilio.rest import Client
 
-def URL(url):
-    return url
+def sendMessage(toNumber, fromNumber, message):
+    account_sid = "AC9b64644cb7c33ef1467ab5fba7aa385f"
 
-    def sendMessage(toNumber, fromNumber):
-        account_sid = "AC9b64644cb7c33ef1467ab5fba7aa385f"
+    auth_token  = "b88f5ffb3a8fe815538506ebed83b3d6"
 
-        auth_token  = "b88f5ffb3a8fe815538506ebed83b3d6"
+    client = Client(account_sid, auth_token)
 
-        client = Client(account_sid, auth_token)
+    message = client.messages.create(
+        to = toNumber,
+        from_ = fromNumber,
+        body = message)
 
-        message = client.messages.create(
-            to = "+17853805890",
-            from_ = "+17165314545",
-            body = URL(url))
-
-print(message.sid)
+sendMessage("7853200582", "7165314545", "Hey Tony")
