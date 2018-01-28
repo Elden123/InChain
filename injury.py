d1a7359854bb9ab7ec544f4d25824045dcc62aff
Unknown <nolanblankenau@gmail.com>
2018-01-28 03:34:12 -0600

    Auto stash before merge of "master" and "origin/master"

--- injury.py
+++ injury.py
@@ -5,6 +5,7 @@ class injury:
         self.patientFistName = data['patientfirstname']
         self.patientLastName = data['patientlastname']
         self.diagnosis = data['diagnosis']
+        self.diagnosisDate = data['diagnosisdata']
         self.conditionStart = data['conditionstart']
 
     def getDoctorFirstName():
@@ -20,10 +21,13 @@ class injury:
         return(self.patientLastName)
 
     def getDiagnosis():
-        return (self.diagnosis)
+        return(self.diagnosis)
+
+    def getDiagnosisDate():
+        return(self.diagnosisDate)
 
     def getConditionStart():
         return(self.conditionStart)
 
     def createHTML():
-        html = {"html", "<!doctype html> <html> <head> <meta charset='UTF-8'> <title>InChain</title> <script> function handleThings() { alert(1); } </script> </head> <body> <div id='form' onsubmit='return false'> <h1 class='mdc_typography--display3' id='data'> Form </h1> <form> <div id='doctorfirstname'> First Name of Doctor: " + getDoctorFirstName() + " <input type='text' name = 'doctorfirstname'><br> </div> <div id='doctorlastname'> Last Name of Doctor: " + getDoctorLastName() + " <input type='text' name = 'doctorlastname'><br> </div> <div id='patientfirstname'> First Name of Patient: " + getPatientFirstName() + " <input type='text' name = 'patientfirstname'><br> </div> <div id='patientlastname'> Last Name of Patient: " + getPatientLastName() + " <input type='text' name = 'patientlastname'><br> </div> <div id='diagnosis'> " + getDiagnosis() + " Diagnosis: <input type='text' name = 'diagnosis'><br> </div> <div id='diagnosisdate'> Date of Diagnosis: " + getConditionStart() + " <input type='text' name = 'diagnosisdate'><br> </div> <button onclick='handleThings()' type='submit'>Submit</button> </form> </div> </body> </html> "}
+        html = {, "<!doctype html> <html> <head> <meta charset='UTF-8'> <title>InChain</title> <script> function handleThings() { alert(1); } </script> </head> <body> <div id='form' onsubmit='return false'> <h1 class='mdc_typography--display3' id='data'> Form </h1> <form> <div id='doctorfirstname'> First Name of Doctor: " + getDoctorFirstName() + " <input type='text' name = 'doctorfirstname'><br> </div> <div id='doctorlastname'> Last Name of Doctor: " + getDoctorLastName() + " <input type='text' name = 'doctorlastname'><br> </div> <div id='patientfirstname'> First Name of Patient: " + getPatientFirstName() + " <input type='text' name = 'patientfirstname'><br> </div> <div id='patientlastname'> Last Name of Patient: " + getPatientLastName() + " <input type='text' name = 'patientlastname'><br> </div> <div id='diagnosis'> " + getDiagnosis() + " Diagnosis: <input type='text' name = 'diagnosis'><br> </div> <div id='diagnosisdate'> Date of Diagnosis: " + getConditionStart() + " <input type='text' name = 'diagnosisdate'><br> </div> <button onclick='handleThings()' type='submit'>Submit</button> </form> </div> </body> </html> "}
